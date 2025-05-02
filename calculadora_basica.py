e42c44845214fb4101c5c26c9bb5d25be8e799ac
Ariosto <karenmoralesmorales1313@gmail.com>
2025-05-02 15:59:53 -0500

    Agregar funciones

--- calculadora_basica.py
+++ calculadora_basica.py
@@ -4,6 +4,15 @@ def suma(a,b):
 def resta(a,b):
     return a - b
 
+def multiplicacion(a,b):
+    return a * b
+
+def division(a,b):
+    if b != 0:
+        return a / b
+    else:
+        return "Error: División por cero no permitida."
+
 def main():
     print("Calculadora Básica")
     print("1. Sumar")
